835278dc1be51de2ec9cff75c7052c3756f6ed0a
Mariya <mariya.poltoratskaya@gmail.com>
2018-09-20 00:32:10 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if (currency > 10000) {
+    return  { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  }
+
+  if (currency <= 0) {
+    return {};
+  }
+
+    var countH = 0, //50¢
+        countQ = 0, //25¢
+        countD = 0, //10¢
+        countN = 0, //5¢
+        countP = 0; //1¢
+
+  while (currency >= 50) {
+    currency -= 50;
+    countH++;
+  }
+
+  while (currency >= 25) {
+    currency -= 25;
+    countQ++;
+  }
+
+  while (currency >= 10) {
+    currency -= 10;
+    countD++;
+  }
+
+  while (currency >= 5) {
+    currency -= 5;
+    countN++;
+  }
+
+  while (currency >= 1) {
+    currency -= 1;
+    countP++;
+  }
+
+  var exchange = new Object();
+  if (countH) exchange.H = countH;
+  if (countQ) exchange.Q = countQ;
+  if (countD) exchange.D = countD;
+  if (countN) exchange.N = countN;
+  if (countP) exchange.P = countP;
+
+  return exchange;
 }
